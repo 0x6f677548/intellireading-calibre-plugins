8d04db64f1fc614f1e228fdd74808b319702a946
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2025-05-03 07:42:35 +0100

    chore: correct version number

--- epubmg_filetypeplugin/__init__.py
+++ epubmg_filetypeplugin/__init__.py
@@ -22,7 +22,7 @@ class MetaguidedEpubFileType(FileTypePlugin):
     description = "Works on post conversion/import and converts epub files to a metaguided format, improving your focus and reading speed (sometimes called bionic reading)."
     supported_platforms = ["windows", "osx", "linux"]
     author = "0x6f677548 (Hugo Batista)"
-    version = (1, 1, 1)
+    version = (1, 1, 0)
     file_types = set(["epub"])
     minimum_calibre_version = (6, 5, 0)
     on_postprocess = True  # Run this plugin after conversion is complete
