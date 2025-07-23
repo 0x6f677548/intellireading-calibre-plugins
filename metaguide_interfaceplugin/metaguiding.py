1e5f13c1628c672ed515bfbd2e547ba3f523dd9e
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2025-07-23 07:51:48 +0100

    bump metaguiding.py from cli ( fix: bypass nav.xhtml from metaguiding, as this affects kobo rendering)

--- metaguide_interfaceplugin/metaguiding.py
+++ metaguide_interfaceplugin/metaguiding.py
@@ -218,10 +218,9 @@ class _EpubItemFile:
 
         # some epub have files with html extension but they are xml files
         self.is_xhtml_document = _extension in _XHTML_EXTENSIONS
-        
         # check whether the file is a table of contents or navigation file
         # by checking the filename against a list of known TOC filenames
-        # this is a heuristic and may not be 100% accurate. 
+        # this is a heuristic and may not be 100% accurate.
         # Separate the directory check from the filename check
         self.is_toc_document = False
         if self.filename:
